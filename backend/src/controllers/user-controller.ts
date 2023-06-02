a2ee4c544a6b1fa5562471a18f650c16ad9c0741
pedrorosadomingues@gmail.com <pedrorosadomingues@gmail.com>
2023-06-02 00:45:26 -0300

    fix: error message

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -12,8 +12,8 @@ export async function userPost(req: Request, res: Response): Promise<Response> {
       .send({ id: user.id, name: user.name, email: user.email });
   } catch (error) {
     if (error.name === "DuplicateUserError") {
-      return res.status(httpStatus.CONFLICT).send({ message: error.message });
+      return res.status(httpStatus.CONFLICT).send(error.message);
     }
-    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send();
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
   }
 }
