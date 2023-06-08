56f0df19afb69b2ad6c1e66b4e2227f4ba92dd72
pedrorosadomingues@gmail.com <pedrorosadomingues@gmail.com>
2023-06-08 20:25:23 -0300

    feat: login and register pages

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+
+export default function SignIn() {
+    return (
+        <main className="flex w-full h-screen items-center justify-center column flex-col"
+        >
+            <h1 className='text-red-900 font-black'>Entrou no sistema</h1>
+          
+        </main>
+    )
+}
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,10 +1,10 @@
-import Link from 'next/link'
-import { useState } from 'react'
-import api from '../config/server'
-import { w } from "windstitch"
+import Link from 'next/link';
+import { useState } from 'react';
+import api from '../config/server';
+import { w } from "windstitch";
 
-export default function SignIn() {
-    const [form, setForm] = useState({ email: '', password: '' })
+export default function SignUp() {
+    const [form, setForm] = useState({ name: '', email: '', password: '' })
     function handleChange(e) {
         const { name, value } = e.target
         setForm({ ...form, [name]: value })
@@ -12,19 +12,24 @@ export default function SignIn() {
     async function handleSubmit(e) {
         e.preventDefault()
         try {
-            const { data } = await api.post('/sign-in', form)
-            localStorage.setItem('token', data.token)
-            window.location.href = '/Home'
+            await api.post('/user/sign-up', form)
+            alert('Cadastrado com sucesso')
+            window.location.href = '/'
         } catch (error) {
             console.log(error.response.data.message)
             alert(error.response.data.message)
         }
     }
     return (
-        <SignInCtn
+        <SignUpCtn
         >
             <Title>ClassPerformance</Title>
             <Form onSubmit={handleSubmit} >
+                <Label>Nome</Label>
+                <Input onChange={handleChange}
+                    name="name"
+                    type="text"
+                    value={form.name} />
                 <Label>Email</Label>
                 <Input onChange={handleChange}
                     name="email"
@@ -35,10 +40,10 @@ export default function SignIn() {
                     name="password"
                     type="password"
                     value={form.password} />
-                <Button type="submit">Entrar</Button>
-                <P>Não tem conta? <LinkStl href="/SignUp">Cadastre-se</LinkStl></P>
+                <Button type="submit">Cadastrar</Button>
+                <P>Já tem conta? <LinkStl href="/SignIn">Entre</LinkStl></P>
             </Form>
-        </SignInCtn>
+        </SignUpCtn>
     )
 }
 
@@ -48,7 +53,7 @@ const Input = w.input`
 const Label = w.label`
   text-left`;
 
-const SignInCtn = w.main`
+const SignUpCtn = w.main`
 flex w-full h-screen items-center justify-center column flex-col`;
 
 const Title = w.h1`
@@ -57,11 +62,11 @@ const Title = w.h1`
 const Button = w.button`
 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`;
 
-const Form = w.form`
-flex flex-col w-1/3`;
-
 const P = w.p`
-  text-center text-gray-500 text-sm mt-4`;
+text-center mt-4`;
 
 const LinkStl = w.a`
-  text-blue-500 hover:text-blue-700 text-sm hover:font-bold`;
+text-blue-500 hover:text-blue-700 text-left`;
+
+const Form = w.form`
+flex flex-col w-1/3`;
