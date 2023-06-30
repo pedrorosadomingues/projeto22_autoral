172c6bb7d64da9417e31382e362266b9a2c4d376
Pedro Rosa <pedrorosadomingues@gmail.com>
2023-06-30 20:28:05 -0300

    fix: docker

--- frontend/src/components/Form/WeekdayButtons.js
+++ frontend/src/components/Form/WeekdayButtons.js
@@ -14,11 +14,8 @@ export default function WeekdayButtons({ handleChange, weekdayId }) {
                         <input className="cursor-pointer" onClick={(e) => {
                             handleChange(e)
                             setCheckedState(false)
-<<<<<<< HEAD
-                            }}  type="radio" name="weekdayId" value={weekday.id} />
-=======
                             }} type="radio" name="weekdayId" value={weekday.id} />
->>>>>>> 66f21d1b9e4e995aff4f440fa769b9ec6e8ed476
+
                         <label className="cursor-pointer text-[12px]" >{weekday.day}</label>
                     </WeekdayButtonsStl>
                 ))}
